577b6733699efa32406237ccf745dd7295d50278
Grace Info Media <graceinfomediaonline@gmail.com>
2026-06-17 20:38:00 +0530

    added main plugin file

---
+++ certificate-verification.php
@@ -0,0 +1,19 @@
+<?php
+
+// If this file is called directly, abort.
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly.
+}
+/*
+ * Plugin Name:       WP Certificate Verification
+ * Plugin URI:        https://graceinfomedia.com
+ * Description:       Allow schools and colleges to verify issued certificates on their website.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Grace Info Media
+ * Author URI:        https://graceinfomedia.com
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       wp-certificate-verification
+ */
