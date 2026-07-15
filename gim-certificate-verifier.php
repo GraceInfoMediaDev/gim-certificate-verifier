c994e612e6723d0a35bbbe27ab0b9829c8a4a7f9
Grace Info Media <graceinfomediaonline@gmail.com>
2026-07-15 08:07:06 +0530

    Revert "Changed description"
    This reverts commit 2ba79ca725c0f4bd2327dcb8514eb2fa95776528.

--- gim-certificate-verifier.php
+++ gim-certificate-verifier.php
@@ -2,7 +2,7 @@
 /*
  * Plugin Name:       GIM Certificate Verifier
  * Plugin URI:        https://graceinfomedia.com
- * Description:       Allow schools & colleges to verify issued certificates on their website.
+ * Description:       Allow schools and colleges to verify issued certificates on their website.
  * Version:           1.0.0
  * Requires at least: 5.2
  * Requires PHP:      7.2
